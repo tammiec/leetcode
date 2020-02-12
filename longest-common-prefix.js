a120c918992b9cc10fe593ddd7bbcd0782a20bc7
Tammie Chung <tammie.chung11@gmail.com>
2020-02-11 19:42:13 -0500

    Solve longest common prefix

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -6,9 +6,19 @@ var longestCommonPrefix = function(strs) {
 
   let prefix = '';
 
+  if (strs.length === 0) {
+    return prefix;
+  }
+
   for (let i = 0; i < strs[0].length; i++) {
 
-    
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) {
+        return prefix;
+      }
+    }
+
+    prefix += strs[0][i];
 
   }
 
